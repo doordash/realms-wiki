2d5fe720c2f4fa6dfebbb5b68352d629635f5ebf
Thomas Ptacek <tqbf@KEDZIE.local>
2017-03-29 14:46:00 -0500

    apply same change

--- realms/modules/search/views.py
+++ realms/modules/search/views.py
@@ -1,10 +1,14 @@
 from flask import abort, g, render_template, request, redirect, Blueprint, flash, url_for, current_app
 from realms import search as search_engine
+from flask.ext.login import login_required, current_user
 
 blueprint = Blueprint('search', __name__)
 
 
 @blueprint.route('/_search')
 def search():
+    if current_app.config.get('PRIVATE_WIKI') and current_user.is_anonymous():
+        return current_app.login_manager.unauthorized()
+
     results = search_engine.wiki(request.args.get('q'))
     return render_template('search/search.html', results=results)
